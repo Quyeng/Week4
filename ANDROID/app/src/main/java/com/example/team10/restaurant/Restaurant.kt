832ae53dc5b5c6f34a6f68228b3912f168f88f33
vophungquang <vophungquang123@gmail.com>
2021-04-23 11:10:05 +0700

    fix AndroidManifest

--- ANDROID/app/src/main/java/com/example/team10/restaurant/Restaurant.kt
+++ ANDROID/app/src/main/java/com/example/team10/restaurant/Restaurant.kt
@@ -1,4 +1,4 @@
 package com.example.team10.restaurant
 
-class Restaurant( val res_name: String, val address: String, val avatar: String)  {
+data class Restaurant( val res_name: String, val address: String, val avatar: String)  {
 }
